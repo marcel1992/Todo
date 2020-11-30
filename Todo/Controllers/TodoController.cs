916b707c889159bc61be29937913463352fb7d07
valach marcel <www.valachmarcel@gmail.com>
2020-11-30 15:59:46 +0200

    refactor urls

--- Todo/Controllers/TodoController.cs
+++ Todo/Controllers/TodoController.cs
@@ -17,27 +17,27 @@ namespace Todo.Controllers
             _repository = repository;
         }
 
-        [HttpGet("~/api/GetTodos")]
+        [HttpGet("~/api/getAll")]
         public async Task<ActionResult<IReadOnlyList<TodoModel>>> GetTodosAsync() => Ok(await _repository.GetAllAsync());
 
-        [HttpGet("~/api/GetTodo/{id}")]
+        [HttpGet("~/api/get/{id}")]
         public async Task<ActionResult<TodoModel>> GetTodoAsync(int id) => Ok(await _repository.GetByIdAsync(id));
 
-        [HttpPost("~/api/AddTodo")]
+        [HttpPost("~/api/add")]
         public async Task<ActionResult> AddTodoAsync([FromBody] TodoModel newTodo)
         {
             await _repository.AddAsync(newTodo);
             return Ok();
         }
 
-        [HttpPut("~/api/UpdateTodo")]
+        [HttpPut("~/api/update")]
         public async Task<ActionResult> UpdateTodoAsync([FromBody] TodoModel modifiedTodo)
         {
             await _repository.UpdateAsync(modifiedTodo);
             return Ok();
         }
 
-        [HttpDelete("~/api/DeleteTodo/{id}")]
+        [HttpDelete("~/api/delete/{id}")]
         public async Task<ActionResult> DeleteTodoAsync(int id)
         {
             await _repository.DeleteAsync(id);
